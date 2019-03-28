bc972d889bb73a790b1c9e9eed8c11b1a95a8794
MrManYoYo <sky_variant@126.com>
2019-03-28 16:35:24 +0800

    30% single-line-chart

---
+++ src/components/pages/line/single-line.vue
@@ -0,0 +1,38 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    chartId: {
+      type: String,
+      default: 'singleLine',
+    },
+    source: {
+      type: [Array, Object],
+      default: () => [
+        { name: 'item0', value: 10 },
+        { name: 'item1', value: 20 },
+        { name: 'item2', value: 30 },
+        { name: 'item3', value: 40 },
+        { name: 'item4', value: 40 },
+        { name: 'item5', value: 30 },
+        { name: 'item6', value: 20 },
+        { name: 'item7', value: 10 },
+      ],
+    },
+  },
+  data() {
+    return {
+      chart: null,
+      
+    }
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
